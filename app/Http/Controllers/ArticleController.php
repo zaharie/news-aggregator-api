3dd66d917b85ffcc26e649e6e16bd290870d75d4
zaharie <zahariecont@hotmail.com>
2024-09-29 20:52:36 -0300

    added qery by datestart dateEnd

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -11,16 +11,30 @@ class ArticleController extends Controller
     {
         $author = $request->query('author');
         $category = $request->query('category');
+        $startDate = $request->query('start_date');
+        $endDate = $request->query('end_date');     
         $page = $request->query('page', 1);
+    
         $query = Article::query();
+
         if ($author) {
             $query->where('author', 'ilike', '%' . $author . '%');
         }
-
+    
         if ($category) {
             $query->where('category', 'ilike', '%' . $category . '%');
         }
+    
+        if ($startDate && $endDate) {
+            $query->whereBetween('published_at', [$startDate, $endDate]);
+        } elseif ($startDate) {
+            $query->where('published_at', '>=', $startDate);
+        } elseif ($endDate) {
+            $query->where('published_at', '<=', $endDate);
+        }
+    
         $articles = $query->paginate(50, ['*'], 'page', $page);
+    
         return response()->json($articles);
     }
 
